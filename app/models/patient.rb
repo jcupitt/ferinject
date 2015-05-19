6654c5e30a0faf7f214343721a2529d9f6a33b12
John Cupitt <jcupitt@gmail.com>
2015-05-19 11:41:32 +0100

    default screening date
    also unique screening number

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,17 @@
+require 'date'
+
 class Patient < ActiveRecord::Base
     validates :initials, presence: true, length: { minimum: 2 }
     validates :date_of_birth, presence: true, date: true
-    validates :screening_number, numericality: { only_integer: true }
+    validates :screening_number, 
+        numericality: { only_integer: true }, uniqueness: true
     validates :screening_date, presence: true, date: true
+
+    after_initialize do
+        if self.new_record?
+            # only happens for .new
+            self.screening_date = Date.today.to_s
+        end
+    end
+
 end
