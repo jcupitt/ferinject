23946fc25838541f0a427afb2ab50037c6725216
John Cupitt <jcupitt@gmail.com>
2015-05-21 16:12:05 +0100

    add cast to help progresql

--- db/migrate/20150521150119_change_screening_number_in_patients.rb
+++ db/migrate/20150521150119_change_screening_number_in_patients.rb
@@ -1,5 +1,5 @@
 class ChangeScreeningNumberInPatients < ActiveRecord::Migration
   def change
-      change_column :patients, :screening_number, :integer, :default => 0
+      change_column :patients, :screening_number, 'integer USING CAST(column_name AS integer)', :default => 0
   end
 end
